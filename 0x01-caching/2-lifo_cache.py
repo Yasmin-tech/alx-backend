462364ee43d5ae427e810ebc6f4267393b85fdfe
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-08-21 15:41:20 +0300

    add task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+''' a class LIFOCache implementation that inherits from
+    BaseCaching and is a caching system
+    '''
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    ''' A LIFO caching system.
+
+        This caching system has a limit MAX_ITEMS = 4
+
+        The last added item to self.cache_data will be removed
+
+        Instance Method:
+        put: to add key value pair,
+            If the number of items in self.cache_data is higher
+            than BaseCaching.MAX_ITEMS,
+            discard the last item put in cache (LIFO algorithm)
+            and print DISCARD: with the key discarded.
+        get: to retrive the value from the system
+        '''
+    def __init__(self) -> None:
+        ''' inisialize an instance
+            '''
+        super().__init__()
+
+    def put(self, key: str, value: any) -> None:
+        ''' add key value pair,
+            If the number of items in self.cache_data is higher
+            than BaseCaching.MAX_ITEMS,
+            discard the first item put in cache (FIFO algorithm)
+            and print DISCARD: with the key discarded.
+            '''
+
+        if key and value:
+            if len(self.cache_data) >= self.MAX_ITEMS \
+                    and key not in self.cache_data:
+                # get the last item added if the system is full and remove it
+                # sorted() function returns a list
+                last_key = sorted(self.cache_data.keys())[-1]
+                del self.cache_data[last_key]
+                print('DISCARD:', last_key)
+            self.cache_data[key] = value
+
+    def get(self, key: str) -> any:
+        ''' Retrieve the associated value if the key is not none
+            and exists in the self.cache_data. Otherwise, return None
+            '''
+        if key:
+            return self.cache_data.get(key)
+        return None
