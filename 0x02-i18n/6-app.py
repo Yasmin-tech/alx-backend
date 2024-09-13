1bcdf0f0fcd35a72ee2e726131529cc0f60a268b
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-14 02:51:23 +0300

    add task 6 again

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -3,7 +3,7 @@
 
 from flask_babel import Babel, gettext
 from flask import Flask, render_template, request, g
-from typing import Dict, Optional
+from typing import Dict, Union
 
 
 class Config:
@@ -40,7 +40,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(supported_lang)
 
 
-def get_user() -> Optional[Dict]:
+def get_user() -> Union[Dict]:
     """
         If the variable login_as is set
         and found in users, this function return
